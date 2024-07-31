8a6c90474821fae9bd6a19f85425eb107063ab48
Namrata-57 <namrata57desai@gmail.com>
2024-08-01 03:01:37 +0530

    Chnages Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import {priceCardsData} from './data.js'
-import PriceCard from './PriceCard';
-import './App.css';
+import { priceCardsData } from "./data.js";
+import PriceCard from "./PriceCard";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App m-5">
-      <h4>React Price Card Task</h4>
-      <div className="price-cards-container bg-primary mt-5">
+      <div className="price-cards-container bg-primary">
         {priceCardsData.map((card, index) => (
           <PriceCard
             key={index}
--- src/PriceCard.jsx
+++ src/PriceCard.jsx
@@ -1,34 +1,54 @@
-import PropTypes from 'prop-types';
-import './PriceCard.css';
+import PropTypes from "prop-types";
+import "./PriceCard.css";
+import { FaCheck } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
 
 const PriceCard = ({ type, price, features }) => {
-    const alertMessage= ()=>{
-        alert(`Amazing! You have selected the ${type} plan.`) //alert message will be displayed 
-    }
+  const alertMessage = () => {
+    alert(`Amazing! You have selected the ${type} plan.`);
+  };
+
   return (
-    <div className="price-card bg-light">
+    <div className="price-card">
       <h6 className="customType">{type}</h6>
       <p className="price">{price}</p>
-      <hr className='lighterHr'/>
+      <hr className="lighterHr" />
       <ul className="features">
         {features.map((feature, index) => (
-          <li key={index} className={feature.isDisabled ? 'disabled-feature' : ''}>
-            <i className={feature.isDisabled ? 'bi bi-x' : 'bi bi-check'}></i>{feature.text}</li>
+          <li
+            key={index}
+            className={feature.isDisabled ? "disabled-feature" : ""}
+          >
+            <span
+              className={`check-mark ${
+                feature.isDisabled ? "disabled-check" : "bold-check"
+              }`}
+            >
+              {feature.isDisabled ? <FaTimes /> : <FaCheck />}
+            </span>
+            {feature.text}
+          </li>
         ))}
       </ul>
-      <button onClick={alertMessage}className="buttonStyle btn btn-primary">BUTTON</button>
+      <button
+        onClick={alertMessage}
+        className="buttonStyle btn btn-primary py-2"
+      >
+        BUTTON
+      </button>
     </div>
   );
 };
+
 PriceCard.propTypes = {
-    type: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    features: PropTypes.arrayOf(
-        PropTypes.shape({
-        text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  features: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
       isDisabled: PropTypes.bool.isRequired,
     })
-    ).isRequired,
-  };
-  
+  ).isRequired,
+};
+
 export default PriceCard;
